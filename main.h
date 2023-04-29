80543b3b6ca07c5e1ddc8cff1ce2f9885e0bc357
Ogagayefe Success <successogagayefe20@gmail.com>
2023-04-28 19:07:58 -0700

    Made some correction in the main

--- main.h
+++ main.h
@@ -7,14 +7,14 @@
 #define UNUSED(x) (void)(x)
 #define BUFF_SIZE 1024
 
-/* FLAGS */
+/* Flags */
 #define F_MINUS 1
 #define F_PLUS 2
 #define F_ZERO 4
 #define F_HASH 8
 #define F_SPACE 16
 
-/* SIZES */
+/* Sizes */
 #define S_LONG 2
 #define S_SHORT 1
 
